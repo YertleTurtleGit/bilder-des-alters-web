5322c91a1a5070379081c4c002900c393013fbde
YertleTurtleGit <github@galzone.de>
2020-11-28 14:15:41 +0100

    responsive

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 "use strict";
 const CUBE = document.getElementById("cube");
 const MENU_ITEMS = document.getElementsByClassName("menu-item");
+const CONTENT_PARTS = document.getElementsByClassName("content-part");
 var currentMenuItemIndex = 0;
 var introRotated = false;
-function wheeling() {
+function wheeling(event) {
     if (!introRotated) {
         setTimeout(function () {
             introRotated = true;
@@ -13,7 +14,20 @@ function wheeling() {
         changeIndex(0);
     }
     else {
-        nextIndex();
+        if (event.deltaY < 0) {
+            previousIndex();
+        }
+        else {
+            nextIndex();
+        }
+    }
+}
+function previousIndex() {
+    if (currentMenuItemIndex - 1 < 0) {
+        changeIndex(MENU_ITEMS.length - 1);
+    }
+    else {
+        changeIndex(currentMenuItemIndex - 1);
     }
 }
 function nextIndex() {
@@ -28,10 +42,19 @@ function changeIndex(newIndex) {
     currentMenuItemIndex = newIndex;
     for (var i = 0; i < MENU_ITEMS.length; i++) {
         let drawingBuffer = MENU_ITEMS[i];
+        let contentDrawingBuffer = CONTENT_PARTS[i];
         drawingBuffer.style.backgroundColor = "white";
+        contentDrawingBuffer.style.display = "none";
     }
     let drawingBuffer = MENU_ITEMS[newIndex];
+    let contentDrawingBuffer = CONTENT_PARTS[newIndex];
     drawingBuffer.style.backgroundColor = "lightgray";
+    contentDrawingBuffer.style.display = "block";
 }
 window.addEventListener("wheel", wheeling);
+for (var i = 0; i < MENU_ITEMS.length; i++) {
+    MENU_ITEMS[i].id = "menu-item-" + i;
+    CONTENT_PARTS[i].id = "content-part-" + i;
+    MENU_ITEMS[i].addEventListener("click", changeIndex.bind(null, i));
+}
 console.log("script loaded.");
--- script.ts
+++ script.ts
@@ -2,11 +2,14 @@
 
 const CUBE = document.getElementById("cube");
 const MENU_ITEMS: HTMLCollection = document.getElementsByClassName("menu-item");
+const CONTENT_PARTS: HTMLCollection = document.getElementsByClassName(
+   "content-part"
+);
 
 var currentMenuItemIndex: number = 0;
 
 var introRotated: boolean = false;
-function wheeling(): void {
+function wheeling(event): void {
    if (!introRotated) {
       setTimeout(function () {
          introRotated = true;
@@ -15,7 +18,19 @@ function wheeling(): void {
       CUBE.style.transform = "translateZ(-100px)";
       changeIndex(0);
    } else {
-      nextIndex();
+      if (event.deltaY < 0) {
+         previousIndex();
+      } else {
+         nextIndex();
+      }
+   }
+}
+
+function previousIndex(): void {
+   if (currentMenuItemIndex - 1 < 0) {
+      changeIndex(MENU_ITEMS.length - 1);
+   } else {
+      changeIndex(currentMenuItemIndex - 1);
    }
 }
 
@@ -32,13 +47,22 @@ function changeIndex(newIndex: number): void {
 
    for (var i = 0; i < MENU_ITEMS.length; i++) {
       let drawingBuffer = MENU_ITEMS[i] as HTMLElement;
+      let contentDrawingBuffer = CONTENT_PARTS[i] as HTMLElement;
       drawingBuffer.style.backgroundColor = "white";
+      contentDrawingBuffer.style.display = "none";
    }
 
    let drawingBuffer = MENU_ITEMS[newIndex] as HTMLElement;
+   let contentDrawingBuffer = CONTENT_PARTS[newIndex] as HTMLElement;
    drawingBuffer.style.backgroundColor = "lightgray";
+   contentDrawingBuffer.style.display = "block";
 }
 
 window.addEventListener("wheel", wheeling);
+for (var i = 0; i < MENU_ITEMS.length; i++) {
+   MENU_ITEMS[i].id = "menu-item-" + i;
+   CONTENT_PARTS[i].id = "content-part-" + i;
+   MENU_ITEMS[i].addEventListener("click", changeIndex.bind(null, i));
+}
 
 console.log("script loaded.");

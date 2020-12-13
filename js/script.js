9ee831d46332f56d30f929f9498f70a511ea5efd
YertleTurtleGit <github@galzone.de>
2020-12-13 20:38:06 +0100

    dfsjf

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function getSceneHeight(sceneId) {
     return SCENES[sceneId].scrollHeight;
 }
 function getActiveSceneId(scrollY) {
-    var heightOffset = 0;
+    var heightOffset = -getCubeSize();
     for (var i = 0; i < SCENES.length; i++) {
         heightOffset += getSceneHeight(i);
         if (scrollY < heightOffset) {
@@ -22,7 +22,44 @@ function getActiveSceneId(scrollY) {
     return SCENES.length - 1;
 }
 function setActiveScene(sceneId) {
-    console.log(sceneId);
+    const backFaces = [];
+    const cubes = [];
+    for (var i = 0; i < SCENES.length; i++) {
+        cubes.push(SCENES[i].getElementsByClassName("cube")[0]);
+        backFaces.push(SCENES[i].getElementsByClassName("cube__face--back")[0]);
+    }
+    const faces = cubes[sceneId].children;
+    cubes[sceneId].style.transform = "translateZ(-100px)";
+    cubes[sceneId].style.zIndex = "2";
+    backFaces[sceneId].style.zIndex = "1";
+    backFaces[sceneId].style.transform =
+        "rotateY(180deg) translateZ(" + String(getCubeSize() / 2) + "px)";
+    for (var j = 0; j < faces.length; j++) {
+        const faceContents = faces[j].querySelectorAll("p,li");
+        for (var k = 0; k < faceContents.length; k++) {
+            let faceContentBuffer = faceContents[k];
+            faceContentBuffer.style.backgroundColor = "lime";
+            faceContentBuffer.style.zIndex = "2";
+        }
+    }
+    for (var i = 0; i < cubes.length; i++) {
+        const faces = cubes[i].children;
+        if (sceneId !== i) {
+            cubes[i].style.transform = "translateZ(-100px) rotateY(-90deg)";
+            cubes[i].style.zIndex = "1";
+            backFaces[i].style.zIndex = "1";
+            backFaces[i].style.transform =
+                "rotateY(90deg) translateZ(" + String(getCubeSize() / 2) + "px)";
+            for (var j = 0; j < faces.length; j++) {
+                const faceContents = faces[j].querySelectorAll("p,li");
+                for (var k = 0; k < faceContents.length; k++) {
+                    let faceContentBuffer = faceContents[k];
+                    faceContentBuffer.style.backgroundColor = "transparent";
+                    faceContentBuffer.style.zIndex = "1";
+                }
+            }
+        }
+    }
 }
 var currentId = 0;
 window.addEventListener("scroll", (event) => {
--- script.ts
+++ script.ts
@@ -15,7 +15,7 @@ function getSceneHeight(sceneId: number): number {
 }
 
 function getActiveSceneId(scrollY: number): number {
-   var heightOffset: number = 0;
+   var heightOffset: number = -getCubeSize();
    for (var i = 0; i < SCENES.length; i++) {
       heightOffset += getSceneHeight(i);
       if (scrollY < heightOffset) {
@@ -26,7 +26,51 @@ function getActiveSceneId(scrollY: number): number {
 }
 
 function setActiveScene(sceneId: number): void {
-   console.log(sceneId);
+   const backFaces: HTMLElement[] = [];
+   const cubes: HTMLElement[] = [];
+   for (var i = 0; i < SCENES.length; i++) {
+      cubes.push(SCENES[i].getElementsByClassName("cube")[0] as HTMLElement);
+      backFaces.push(
+         SCENES[i].getElementsByClassName("cube__face--back")[0] as HTMLElement
+      );
+   }
+
+   const faces: HTMLCollection = cubes[sceneId].children;
+   cubes[sceneId].style.transform = "translateZ(-100px)";
+   cubes[sceneId].style.zIndex = "2";
+   backFaces[sceneId].style.zIndex = "1";
+   backFaces[sceneId].style.transform =
+      "rotateY(180deg) translateZ(" + String(getCubeSize() / 2) + "px)";
+
+   for (var j = 0; j < faces.length; j++) {
+      const faceContents = faces[j].querySelectorAll("p,li");
+      for (var k = 0; k < faceContents.length; k++) {
+         let faceContentBuffer: HTMLElement = faceContents[k] as HTMLElement;
+         faceContentBuffer.style.backgroundColor = "lime";
+         faceContentBuffer.style.zIndex = "2";
+      }
+   }
+
+   for (var i = 0; i < cubes.length; i++) {
+      const faces: HTMLCollection = cubes[i].children;
+      if (sceneId !== i) {
+         cubes[i].style.transform = "translateZ(-100px) rotateY(-90deg)";
+         cubes[i].style.zIndex = "1";
+         backFaces[i].style.zIndex = "1";
+         backFaces[i].style.transform =
+            "rotateY(90deg) translateZ(" + String(getCubeSize() / 2) + "px)";
+         for (var j = 0; j < faces.length; j++) {
+            const faceContents = faces[j].querySelectorAll("p,li");
+            for (var k = 0; k < faceContents.length; k++) {
+               let faceContentBuffer: HTMLElement = faceContents[
+                  k
+               ] as HTMLElement;
+               faceContentBuffer.style.backgroundColor = "transparent";
+               faceContentBuffer.style.zIndex = "1";
+            }
+         }
+      }
+   }
 }
 
 var currentId: number = 0;
